85ffe723b9f85057c5ca4b1066d025b27d8cad28
Tourmi <ragtag_devilstick@tourmi.dev>
2022-11-09 02:16:52 -0500

    Operators can now restrict access to the mod
    They can do so with /svmm disable player_name
    Refactor to avoid repeated code, and in preparation for future features

--- src/main/java/dev/tourmi/svmm/commands/Commands.java
+++ src/main/java/dev/tourmi/svmm/commands/Commands.java
@@ -2,28 +2,16 @@ package dev.tourmi.svmm.commands;
 
 import com.mojang.brigadier.CommandDispatcher;
 import com.mojang.brigadier.builder.LiteralArgumentBuilder;
-import com.mojang.brigadier.context.CommandContext;
 import net.minecraft.commands.CommandSourceStack;
-import net.minecraft.network.chat.Component;
-import net.minecraft.world.entity.player.Player;
 
 public class Commands {
     public static void register(CommandDispatcher<CommandSourceStack> dispatcher) {
         LiteralArgumentBuilder<CommandSourceStack> cs = net.minecraft.commands.Commands.literal("svmm")
                 .then(HelpCommand.getCommand())
-                .then(EnableCommand.getCommand())
-                .then(DisableCommand.getCommand())
+                .then(ToggleCommand.getCommand(true))
+                .then(ToggleCommand.getCommand(false))
                 .executes(HelpCommand::execute); //default when no additional arguments given
 
         dispatcher.register(cs);
-    }
-
-    public static void sendMessage(CommandContext<CommandSourceStack> commandContext, String message) {
-        if (commandContext.getSource().getEntity() instanceof Player) {
-            Player player = (Player) commandContext.getSource().getEntity();
-            player.sendSystemMessage(Component.literal(message));
-        } else {
-            commandContext.getSource().getServer().sendSystemMessage(Component.literal(message));
-        }
     }
 }
