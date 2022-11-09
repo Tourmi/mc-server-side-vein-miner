38448e1aaa64902578ae58bd8ebae9c40310191e
Tourmi <ragtag_devilstick@tourmi.dev>
2022-11-09 10:34:53 -0500

    Added mod_disabled config, and configurable default values for clients

--- src/main/java/dev/tourmi/svmm/config/ClientConfig.java
+++ src/main/java/dev/tourmi/svmm/config/ClientConfig.java
@@ -13,9 +13,9 @@ public class ClientConfig {
 
     public ClientConfig() {
         ForgeConfigSpec.Builder builder = new ForgeConfigSpec.Builder();
-        MOD_DISABLED = builder.comment("Whether or not the player disabled the mod for themselves").define("mod_disabled", false);
-        MOD_RESTRICTED = builder.comment("Whether or not the mod was restricted by an operator").define("mod_restricted", SVMMConfig.DEFAULT_RESTRICTED.get().booleanValue());
-        GIANT_VEIN_MINING_DISABLED = builder.comment("Whether or not the player disabled giant vein mining for themselves").define("giant_vein_mining_disabled", false);
+        MOD_DISABLED = builder.comment("Whether or not the player disabled the mod for themselves").define("mod_disabled", SVMMConfig.MOD_DEFAULT_DISABLED.get().booleanValue());
+        MOD_RESTRICTED = builder.comment("Whether or not the mod was restricted by an operator").define("mod_restricted", SVMMConfig.MOD_DEFAULT_RESTRICTED.get().booleanValue());
+        GIANT_VEIN_MINING_DISABLED = builder.comment("Whether or not the player disabled giant vein mining for themselves").define("giant_vein_mining_disabled", SVMMConfig.GIANT_VEIN_MINING_DEFAULT_DISABLED.get().booleanValue());
         GIANT_VEIN_MINING_RESTRICTED = builder.comment("Whether or not giant vein mining was restricted by an operator").define("giant_vein_mining_restricted", SVMMConfig.GIANT_VEIN_MINING_DEFAULT_RESTRICTED.get().booleanValue());
 
         SPEC = builder.build();
--- src/main/java/dev/tourmi/svmm/config/SVMMConfig.java
+++ src/main/java/dev/tourmi/svmm/config/SVMMConfig.java
@@ -10,7 +10,9 @@ public final class SVMMConfig {
 
     public static final ForgeConfigSpec SPEC;
 
-    public static final ForgeConfigSpec.BooleanValue DEFAULT_RESTRICTED;
+    public static final ForgeConfigSpec.BooleanValue MOD_DISABLED;
+    public static final ForgeConfigSpec.BooleanValue MOD_DEFAULT_DISABLED;
+    public static final ForgeConfigSpec.BooleanValue MOD_DEFAULT_RESTRICTED;
     public static final ForgeConfigSpec.IntValue MAXIMUM_BLOCKS_TO_BREAK;
     public static final ForgeConfigSpec.ConfigValue<List<? extends String>> BLOCK_WHITELIST;
     private static final Supplier<List<String>> DEFAULT_BLOCK_WHITELIST = () -> List.of(
@@ -36,6 +38,7 @@ public final class SVMMConfig {
     public static final ForgeConfigSpec.BooleanValue STOP_WHEN_ABOUT_TO_BREAK;
 
     public static final ForgeConfigSpec.BooleanValue GIANT_VEIN_MINING_DISABLED;
+    public static final ForgeConfigSpec.BooleanValue GIANT_VEIN_MINING_DEFAULT_DISABLED;
     public static final ForgeConfigSpec.BooleanValue GIANT_VEIN_MINING_DEFAULT_RESTRICTED;
     public static final ForgeConfigSpec.ConfigValue<List<? extends String>> GIANT_VEIN_STARTER_BLOCKS;
     private static final Supplier<List<String>> DEFAULT_GIANT_VEIN_STARTER_ORE = () -> List.of(
@@ -62,13 +65,16 @@ public final class SVMMConfig {
         ForgeConfigSpec.Builder builder = new ForgeConfigSpec.Builder();
 
         builder.push("Server-side Vein Miner configuration");
-        DEFAULT_RESTRICTED = builder.comment("Whether or not the entire mod is restricted by default for new users").define("default_restricted", false);
+        MOD_DISABLED = builder.comment("Whether or not the entire mod is disabled").define("mod_disabled", false);
+        MOD_DEFAULT_DISABLED = builder.comment("Whether or not the mod is disabled by default for new players.\nThey may enable it with /svmm enable").define("mod_disabled_default", false);
+        MOD_DEFAULT_RESTRICTED = builder.comment("Whether or not the entire mod is restricted by default for new users").define("default_restricted", false);
         MAXIMUM_BLOCKS_TO_BREAK = builder.comment("The maximum amount of blocks the mod is allowed to break at once").defineInRange("maximum_blocks_to_break", 100, 1, 10_000);
         BLOCK_WHITELIST = builder.comment("The blocks that are allowed to be vein-mined").defineList("block_white_list", DEFAULT_BLOCK_WHITELIST.get(), String.class::isInstance);
         STOP_WHEN_ABOUT_TO_BREAK = builder.comment("Whether or not to stop the vein mining when the tool is about to break").define("stop_when_about_to_break_tool", true);
 
         builder.push("Giant vein mining configuration");
         GIANT_VEIN_MINING_DISABLED = builder.comment("Whether or not giant vein mining is disabled on the server").define("giant_vein_mining_disabled", false);
+        GIANT_VEIN_MINING_DEFAULT_DISABLED = builder.comment("Whether or not giant vein mining is disabled by default for new players.\nThey may enable it with /svmm enable giantVein").define("giant_vein_mining_default_disabled", false);
         GIANT_VEIN_MINING_DEFAULT_RESTRICTED = builder.comment("Whether or not giant vein mining is restricted by default for new players").define("giant_vein_mining_default_restricted", false);
         GIANT_VEIN_STARTER_BLOCKS = builder.comment("The blocks the player needs to mine before the vein miner is triggered").defineList("giant_vein_starter_ore", DEFAULT_GIANT_VEIN_STARTER_ORE.get(), String.class::isInstance);
         GIANT_VEIN_WHITELIST = builder.comment("All the blocks the Vein Miner is allowed to mine at once when triggered").defineList("giant_vein_whitelist", DEFAULT_GIANT_VEIN_WHITELIST.get(), String.class::isInstance);
